0b3ad35e7a8fbe042b42ce9514f33ac0b25a1454
zdhsoft <zdhsoft@gmail.com>
2018-11-01 20:41:05 +0800

    - 2018-11-01 - \+ 增加了bitUtil对整数位处理函数，可以用于标志位处理 - \+ 增加utils中的ToInteger, ToFloat, JsonParse和mkdirsSync等几个函数

--- example/test.js
+++ example/test.js
@@ -4,4 +4,17 @@ function testLog() {
 	log.info("hello world");
 }
 
+function testwritedir(paramPath) {
+	let comm = require("../index");
+	let log = comm.getLogger(__filename);
+	let utils = comm.utils;
+	let path = "c:/a/b/c/d/e/f/g";
+	let r = utils.mkdirsSync(path);
+	log.info("make path:" + path + " result=" + r);
+	let v = utils.ToInteger("100",99);
+	let v1 = utils.ToInteger("abcd1111100",99);
+	log.info("v=" + v + ",v1=" + v1);
+}
+
 testLog();
+testwritedir();
