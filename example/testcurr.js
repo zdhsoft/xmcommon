a45cbccb3e1eb40ffc2212ff676f6772bf1ecf60
donghua@shubidatech.com <donghua@shubidatech.com>
2019-06-26 15:59:52 +0800

    优化中文大写的生成

--- example/testcurr.js
+++ example/testcurr.js
@@ -31,7 +31,29 @@ m.push(new CNYCurrency(19.50));
 m.push(new CNYCurrency(6007.14));
 m.push(new CNYCurrency(16409.02));
 m.push(new CNYCurrency(325.04));
+
+function fff(v) {
+    m.push(new CNYCurrency(v));
+}
+fff(0.05);
+let jjj = new CNYCurrency(0.1);
+jjj.Chinese();
+
+fff(1.20);
+fff(1.23);
+fff(20.56);
+fff(15.00);
+fff('56.08');
+fff(200.11);
+fff(205.12);
+fff(5001.09);
+fff(67004.00);
+fff(200080.07);
+fff(0.2);
+fff(0.15);
+
+
 for(let mm of m) {
     let c2 = mm.Chinese();
-    console.log(mm.isErr,mm.value, mm.format(true, true), c2, mm.errMsg);
+    console.log(mm.isErr, mm.value.toFixed(2).padStart(20, ' '), mm.format(true, false).padStart(20,' '), c2, mm.errMsg);
 }
