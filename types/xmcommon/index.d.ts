f92a07c656006b1d20a13c83090d49faa4c940ef
zdhsoft@163.com <zdhsoft>
2020-08-20 09:26:51 +0800

    增加watchRequire注意事项

--- types/xmcommon/index.d.ts
+++ types/xmcommon/index.d.ts
@@ -1024,10 +1024,11 @@ declare module "xmcommon" {
 	/**
 	 * watch指定的js文件，并加载回调
 	 * 这个主要是用于动态加载
+     * @notice 最终于文件名是由path+file组成，对于path不能使用相对路径
 	 * @param paramCallback 文件有变化，加载后的回调函数
-	 * @param paramPath 环境所在的路径
+	 * @param paramPath 环境所在的路径 这里一定要填写绝对路径，否则会侦听不了文件的变化
 	 * @param paramFile 要watch的文件名
-	 * @param paramChangeCallback 有变化才回调
+	 * @param paramChangeCallback 有变化才回调 默认值为true
 	 */
 	function watchRequire(paramCallback:(data:any)=>void, paramPath:string, paramFile:string, paramChangeCallback?:boolean):void;
 
