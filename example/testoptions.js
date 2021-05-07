34a9622037b818ac904dbdd949f769a6d9b46502
zdhsoft@163.com <zdhsoft>
2021-05-08 00:03:59 +0800

    调整库

--- example/testoptions.js
+++
@@ -1,6 +0,0 @@
-let {utils} = require("../lib/utils");
-let v = ["../tt", "--env", "aaaaa", "--version", "1.0.0", "aaaaa", "--flag", "true", "-aaaaa"];
-let k = utils.options(v);
-console.log(JSON.stringify(k, null, 2));
-console.log(JSON.stringify(utils.options(process.argv), null, 2));
-
