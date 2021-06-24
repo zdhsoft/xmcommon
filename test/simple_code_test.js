1778a04bd3c35e9f2923d9263d0da80a42687d1c
zdhsoft <donghua@shubidatech.com>
2021-06-24 16:16:23 +0800

    Create simple_code_test.js

---
+++ test/simple_code_test.js
@@ -0,0 +1,25 @@
+let { SimpleCode } = require('../dist/index');
+let assert = require('assert').strict;
+describe('SimpleCode', () => {
+    // describe('bitUtils.getFlag', ()=> {
+    //     let vv = [];
+
+    //     for (let i = 0; i < EnumBitConst.MAX_BIT_COUNT; i++) {
+    //         vv.push(utils.randomInteger() % 2);
+    //     }
+
+    //     it (`check random bit: ${vv.join("")}`, ()=>{
+
+
+    //         let s = Number.parseInt(vv.join(""), 2);
+    //         let n = 0;
+    //         for(let off = 0; off < vv.length; off++) {
+    //             if (bitUtils.GetFlag(s, vv.length -1 - off) === vv[off]) {
+    //                 n++;
+    //             }
+    //             // console.log(off, bitUtils.GetFlag(s, vv.length -1 - off), vv[off]);
+    //         }
+    //         assert.equal(n, EnumBitConst.MAX_BIT_COUNT);
+    //     });
+    // });
+});
