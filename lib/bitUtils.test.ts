6eb4a8414a34ae74a0bc19e01deb8d0112f96bbf
zdhsoft@163.com <zdhsoft@163.com>
2022-12-08 14:17:49 +0800

    更新readme

--- lib/bitUtils.test.ts
+++ lib/bitUtils.test.ts
@@ -1,7 +1,13 @@
+import { describe, expect, test } from '@jest/globals';
 import { bitUtils } from "./bitUtils";
 
-describe('', ()=> {
+describe('bitUtils 测试', ()=> {
     test('test isValidBitSite', ()=>{
         expect(bitUtils.isValidBitSite(0)).toBe(true);
+        expect(bitUtils.isValidBitSite(-1)).toBe(false);
+        expect(bitUtils.isValidBitSite(31)).toBe(false);
+        expect(bitUtils.isValidBitSite(30)).toBe(true);
+        expect(bitUtils.isValidBitSite(0.1)).toBe(false);
+        expect(bitUtils.isValidBitSite(9.1)).toBe(false);
     });
 });
